5a17128745770deef9c18b62ec2d9f071bd1196a
Shaamil Karim <40500380+shaamil101@users.noreply.github.com>
2022-11-30 23:35:05 -0500

    Update CamPaint.java

--- CamPaint.java
+++ CamPaint.java
@@ -6,7 +6,6 @@ import javax.swing.*;
 
 /**
  * Webcam-based drawing 
- * Scaffold for PS-1, Dartmouth CS 10, Fall 2016
  * 
  * @author Shaamil Shaw Alem, Fall 2022, PS1
  * @author Nicolás Iair Schaievitch, Fall 2022, PS1
